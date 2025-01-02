1bcbe12574737368b99e8bb907199a0d63d1a6fb
Ali-Fares <faresa0202@gmail.com>
2025-01-01 22:08:23 -0500

    reviews carousel

--- src/NavHeader.jsx
+++ src/NavHeader.jsx
@@ -1,19 +1,22 @@
-import React from 'react'
+import React from "react";
 
 function NavHeader() {
-  
   return (
-    <div className='header'>
-      <img className='logo' src="src\images\P X (20).png" alt="logo"/>
-      <div className='hamburger-section'>
-        <a href='#home-section'>Home</a>
-        <a href='#teams-section'>Teams</a>
-        <a href='#about-section'>About</a>
-        <a href='#footer-section'>Contact</a>
+    <div className="header">
+      <a href="#home-section">
+        <img className="logo" src="src/images/P X (20).png" alt="logo" />
+      </a>
+      <div className="hamburger-section">
+        <a href="#home-section">Home</a>
+        <p> | </p>
+        <a href="#teams-section">Teams</a>
+        <p> | </p>
+        <a href="#about-section">About</a>
+        <p> | </p>
+        <a href="#footer-section">Contact</a>
       </div>
-      
     </div>
-  )
+  );
 }
 
-export default NavHeader
+export default NavHeader;
--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
+import { FaFacebook, FaTwitter, FaInstagram } from "react-icons/fa";
 
 function Footer() {
   return (
-    <div id='footer-section'className='footer-container'>Footer</div>
-  )
+    <div id="footer-section" className="footer-container">
+      <div className="socials">
+        <h3>Follow us</h3>
+        <div className="social-handles">
+          <FaFacebook />
+          <FaTwitter />
+          <FaInstagram />
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Footer
+export default Footer;
