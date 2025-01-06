5af82b7feeb7d80aa87535dce37ab3727b7322a5
Ali-Fares <faresa0202@gmail.com>
2025-01-05 22:12:16 -0500

    added dynamic navbar to header and set the basis for the footer styling

--- src/NavHeader.jsx
+++ src/NavHeader.jsx
@@ -1,19 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+import { IoMdMenu, IoMdClose } from "react-icons/io";
 
 function NavHeader() {
+  const [menu, setMenu] = useState(false);
   return (
     <div className="header">
-      <a href="#home-section">
+      <a href="#home-section" style={{ display: "flex", alignItems: "center" }}>
         <img className="logo" src="src/images/P X (20).png" alt="logo" />
       </a>
-      <div className="hamburger-section">
-        <a href="#home-section">Home</a>
-        <p> | </p>
-        <a href="#teams-section">Teams</a>
-        <p> | </p>
-        <a href="#about-section">About</a>
-        <p> | </p>
-        <a href="#footer-section">Contact</a>
+      {!menu && (
+        <div className="hamburger-icon" onClick={() => setMenu(true)}>
+          <IoMdMenu className="hamburger-icon-open" />
+        </div>
+      )}
+      <div className={`header-section ${menu ? "show" : ""}`}>
+        {menu && (
+          <IoMdClose
+            className="hamburger-icon-close"
+            onClick={() => setMenu(false)}
+          />
+        )}
+        <a href="#home-section" onClick={() => setMenu(false)}>
+          Home
+        </a>
+        <a href="#teams-section" onClick={() => setMenu(false)}>
+          Teams
+        </a>
+        <a href="#about-section" onClick={() => setMenu(false)}>
+          About
+        </a>
+        <a href="#footer-section" onClick={() => setMenu(false)}>
+          Contact
+        </a>
       </div>
     </div>
   );
--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -4,8 +4,46 @@ import { FaFacebook, FaTwitter, FaInstagram } from "react-icons/fa";
 function Footer() {
   return (
     <div id="footer-section" className="footer-container">
+      <div className="contact-info">
+        <div>
+          <p
+            style={{
+              display: "inline-block",
+              fontSize: "1.1rem",
+              borderBottom: "1px solid #503076",
+            }}
+          >
+            Contact Us
+          </p>
+          <p>
+            Email:
+            <strong style={{ fontWeight: "500" }}> business@gmail.com</strong>
+          </p>
+          <p>
+            Phone:
+            <strong style={{ fontWeight: "500" }}> 123-123-1234</strong>
+          </p>
+        </div>
+        <div>
+          <p
+            style={{
+              display: "inline-block",
+              fontSize: "1.1rem",
+              borderBottom: "1px solid #503076",
+            }}
+          >
+            Visit Us
+          </p>
+          <p>
+            <strong style={{ fontWeight: "500" }}>
+              1234 Road Brooklyn NY, 11209
+            </strong>
+          </p>
+        </div>
+      </div>
+      <hr className="divider" />
       <div className="socials">
-        <h3>Follow us</h3>
+        <p style={{ fontSize: "1.1rem" }}>Follow us</p>
         <div className="social-handles">
           <FaFacebook />
           <FaTwitter />
