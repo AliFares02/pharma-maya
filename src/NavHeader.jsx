acca9ddc81c91eb4a9a7379541d02ef87a7439c9
Ali-Fares <faresa0202@gmail.com>
2025-01-22 17:43:56 -0500

    changed logo

--- src/NavHeader.jsx
+++ src/NavHeader.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { IoMdMenu, IoMdClose } from "react-icons/io";
-import logo from "/P X (25).png";
+import logo from "/logo.png";
 
 function NavHeader() {
   const [menu, setMenu] = useState(false);
